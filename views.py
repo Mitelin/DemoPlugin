672325207f79ffc06a8f1a0e296c071250c8ee95
Mitelin <Black.k@seznam.cz>
2025-10-02 16:26:09 +0200

    added demo system pro odkazy

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello from demo_plugin!")
+    return render(request, "demo_p"
+                           "lugin/index.html")
