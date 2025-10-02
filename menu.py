a3537080de0039f430efa9caece66a172d331fda
Mitelin <Black.k@seznam.cz>
2025-10-02 15:26:10 +0200

    added demo system pro odkazy

---
+++ menu.py
@@ -0,0 +1,12 @@
+from django.urls import reverse
+
+def get_menu():
+    return {
+        "title": "Demo",
+        "url": "/demo/",  # kořen pluginu (prefix)
+        "items": [
+            {"title": "Úvod", "url": reverse("demo_plugin_index")},
+            # {"title": "Další stránka", "url": reverse("demo_plugin_something")},
+        ],
+    }
+}
