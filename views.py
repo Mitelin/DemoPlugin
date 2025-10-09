f92022371528134669828ee3cd85e07725ab295e
Mitelin <Black.k@seznam.cz>
2025-10-09 12:46:23 +0200

    Update for telemetry usage

--- views.py
+++ views.py
@@ -1,21 +1,4 @@
-from django.http import HttpResponse
-from pathlib import Path
-from telemetry.adapter import emit_event, emit_metric
+from django.shortcuts import render
 
-PLUGIN_NAME = "demo_plugin"
-PLUGIN_DIR = Path(__file__).resolve().parents[0]
-
-def hello_view(request):
-    # event
-    emit_event(
-        "demo.hello_shown", "INFO", "Hello page rendered",
-        labels={"endpoint": "/demo"},
-        plugin_name=PLUGIN_NAME, plugin_dir=PLUGIN_DIR,
-    )
-    # metrika
-    emit_metric(
-        "demo_requests_total", 1, mtype="counter",
-        labels={"endpoint": "/demo"},
-        plugin_name=PLUGIN_NAME, plugin_dir=PLUGIN_DIR,
-    )
-    return HttpResponse("Hello from demo_plugin!")
+def index(request):
+    return render(request, "demo_plugin/index.html")
